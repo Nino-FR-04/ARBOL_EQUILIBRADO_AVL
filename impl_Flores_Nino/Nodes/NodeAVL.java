d1e33ac7d44c41bb5c3a27e40b214720a9eca477
Nino-FR-04 <davidfrx322@gmail.com>
2025-05-18 17:13:36 -0500

    Implementación de la clase NodeAVl que extiende de NodeTree agregando un nuevo atributo

---
+++ impl_Flores_Nino/Nodes/NodeAVL.java
@@ -0,0 +1,28 @@
+package impl_Flores_Nino.Nodes;
+
+/**
+ * Clase que representa el nodo de un BST AVL este contiene el valor
+ * a guardar, referencias a otros dos nodos y el Factor de equilibrio.
+ * @param E valor generico que representa el valor a guardar.
+ */
+public class NodeAVL<E> extends NodeTree<E> {
+
+    //Atributos
+    protected int fe; //Representa el factor de equilibrio
+
+    //Constructor
+    public NodeAVL(E data) {
+        super(data);
+        this.fe = 0;
+    }
+
+    //getter y setter
+    public int getFactEquilibrio() {return this.fe;}
+    public void setFactEquilibrio(int fe) {this.fe = fe;}
+
+    //ToString
+    @Override
+    public String toString() {
+        return "(" + this.data + ", " + this.fe + ")";
+    }
+}
